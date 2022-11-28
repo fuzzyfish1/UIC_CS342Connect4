ec2c07ff06879e4a51bb9350810d429806c005f5
unknown <zain@accucalc.com>
2022-11-27 21:11:01 -0600

    batch file start Script

--- run.bat
+++ run.bat
@@ -1,12 +1,8 @@
 cd C:\Users\Zain\Documents\UIC_CS342Connect4
-echo "temp I hate windows bat shit"
-mvn clean
-echo "completed clean"
-mvn compile
-echo "temp I hate windows bat shit"
+call mvn clean compile
 cd Connect4-Server
-mvn exec:java
+start "" mvn exec:java
 cd ../Connect4-Client
-mvn exec:java
-mvn exec:java
-mvn exec:java
+start "" mvn exec:java
+start "" mvn exec:java
+start "" mvn exec:java
